0a1998d207cec88d9b5741ab1601f0d8981cc10a
Mr Rogers <jon@carbonfive.com>
2018-02-17 21:33:10 +0000

    Add surge and support scss in prod build

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.scss';
 import Navigation from './components/navigation/navigation';
 import Main from './pages/main';
+import ComingSoon from './components/coming_soon/ComingSoon';
 
 class App extends Component {
   render() {
     return (
       <div className="app-container">
-        <Navigation />
-        <Main />
+        <ComingSoon />
       </div>
     );
   }
