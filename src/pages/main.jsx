6e8e0df5c16463301bd09cb6531397983fce07c9
Mr Rogers <jon@carbonfive.com>
2018-02-21 03:44:18 +0000

    Use a clean start main page

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,33 +1,10 @@
 import React, { Component } from 'react'
 
-import ImageBlock from '../components/image_block/image_block';
-
-require("./main.scss");
-
 class Main extends Component {
-
-  images() {
-    const images = [
-      { name: 'image1', url: 'url1' },
-      { name: 'image2', url: 'url21' },
-      { name: 'image1', url: 'url1' },
-      { name: 'image2', url: 'url21' },
-      { name: 'image1', url: 'url1' },
-      { name: 'image2', url: 'url21' },
-      { name: 'image1', url: 'url1' },
-      { name: 'image2', url: 'url21' }
-    ]
-    return images.map((img) => {
-      return <ImageBlock {...img}/>
-    });
-  }
-
   render() {
     return(
       <section className="main">
-        <div className="image_blocks">
-          {this.images()}
-        </div>
+        <h1>Welcome to react</h1>
       </section>
     )
   }
