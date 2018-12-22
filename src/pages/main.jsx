83d1e61b94d32e117a6414c328d43df51cb92b75
Jon Rogers <jon@carbonfive.com>
2018-12-22 15:11:23 -0800

    Better styling of the main component

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -19,6 +19,22 @@ class Main extends Component {
         { id: 7, name: 'image1', src: "http://placekitten.com/120/250", year: 2015 },
         { id: 8, name: 'image2', src: "http://placekitten.com/300/400", year: 2018 },
         { id: 9, name: 'image2', src: "http://placekitten.com/250/200", year: 2010 },
+        { id: 12, name: 'image2', src: "http://placekitten.com/200/400", year: 2010 },
+        { id: 13, name: 'image1', src: "http://placekitten.com/300/300", year: 2011 },
+        { id: 14, name: 'image2', src: "http://placekitten.com/400/200", year: 2012 },
+        { id: 15, name: 'image1', src: "http://placekitten.com/500/400", year: 2013 },
+        { id: 16, name: 'image2', src: "http://placekitten.com/300/400", year: 2014 },
+        { id: 17, name: 'image1', src: "http://placekitten.com/120/250", year: 2015 },
+        { id: 18, name: 'image2', src: "http://placekitten.com/300/400", year: 2018 },
+        { id: 19, name: 'image2', src: "http://placekitten.com/250/200", year: 2010 },
+        { id: 212, name: 'image2', src: "http://placekitten.com/200/400", year: 2010 },
+        { id: 213, name: 'image1', src: "http://placekitten.com/300/300", year: 2011 },
+        { id: 214, name: 'image2', src: "http://placekitten.com/400/200", year: 2012 },
+        { id: 215, name: 'image1', src: "http://placekitten.com/500/400", year: 2013 },
+        { id: 216, name: 'image2', src: "http://placekitten.com/300/400", year: 2014 },
+        { id: 217, name: 'image1', src: "http://placekitten.com/120/250", year: 2015 },
+        { id: 218, name: 'image2', src: "http://placekitten.com/300/400", year: 2018 },
+        { id: 219, name: 'image2', src: "http://placekitten.com/250/200", year: 2010 },
       ]
     };
   }
