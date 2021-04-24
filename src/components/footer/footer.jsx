7fc7dad6c9170041cbdc758608f92543871419e1
Jon Rogers <jon@carbonfive.com>
2021-04-24 10:08:00 -0700

    Update placeholder to have shop link (and move back to react scripts

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 
-require('./footer.scss');
+require("./footer.scss");
 
 const Footer = (props) => {
-  return(
-    <footer >
+  return (
+    <footer>
       <a href="https://www.missionartists.org/artists/mrrogers">
         <div className="footer__content">
           meanwhile check out mr rogers on mission artists
         </div>
       </a>
+      <a href="https://bunnymatic.bigcartel.com">
+        <div className="footer__content">or go buy some art here</div>
+      </a>
     </footer>
   );
-}
+};
 
 export default Footer;
